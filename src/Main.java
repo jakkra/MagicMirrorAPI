e31481eefdbe388b430020439022f01211990538
jakkra <ijakkra@gmail.com>
2016-04-19 16:21:04 +0200

    Getting weather from SMHI is now user friendly :)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,11 @@
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import smhi.Forecasts;
-
-import java.io.IOException;
-import java.net.URL;
+import smhi.SMHIWeatherAPI;
 
 public class Main {
 
     public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            Forecasts forecast = mapper.readValue(new URL("http://opendata-download-metfcst.smhi.se/api/category/pmp2g/version/2/geotype/point/lon/13.191/lat/55.704/data.json"), Forecasts.class);
-            System.out.println(forecast.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SMHIWeatherAPI weatherAPI = new SMHIWeatherAPI("13.191", "55.704");
+        Forecasts forecasts = weatherAPI.getForecasts();
+        System.out.println(forecasts.toString());
     }
 }
