9ada3c8c16a2e87d05d585e4cf3fe4134f849ab7
jakkra <ijakkra@gmail.com>
2016-04-19 13:51:34 +0200

    Fetching weather from SMHI added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import smhi.Forecasts;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Forecasts forecast = mapper.readValue(new URL("http://opendata-download-metfcst.smhi.se/api/category/pmp2g/version/2/geotype/point/lon/13.191/lat/55.704/data.json"), Forecasts.class);
+            System.out.println(forecast.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
